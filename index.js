37bedee93df712c98d9fc1a2f0265636a18af759
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-16 16:11:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
-}
+
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = function funkyFunction()
+var theFunk = funkyFunction()
+}
