3f7b6bcc1690b6379beb56041756ff2aa9198165
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-16 16:03:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function myAnimal() {
 
 function yourAnimal() {
   if (animal !== 'cat') {
-    animal = 'cat';
+   var animal = 'cat';
   }
   // The tests expect this function to return `animal` just like the previous function
   // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
@@ -33,4 +33,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()
+var theFunk = funkyFunction('FUNKY!')
